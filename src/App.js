76338480a5a6ee39c92e3a010adae9382d200c2e
bag3557 <bag3557@gmail.com>
2018-08-17 19:38:55 +0530

    Login functionality with firebase

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
-import AppNavbar from './components/layouts/AppNavbar';
 import Dashboard from './components/layouts/Dashboard';
 import AddClient from './components/clients/AddClient';
 
 import './App.css';
 import ClientDetails from './components/clients/ClientDetails';
 import EditClient from './components/clients/EditClient';
+import Login from './components/auth/Login';
+import AppNavbar from './components/layouts/AppNavbar';
 
 class App extends Component {
   render() {
@@ -25,6 +26,7 @@ class App extends Component {
               <Route exact path="/client/add" component={AddClient} />
               <Route exact path="/client/:id" component={ClientDetails} />
               <Route exact path="/client/edit/:id" component={EditClient} />
+              <Route exact path="/login" component={Login} />
             </Switch>
           </div>
         </div>
